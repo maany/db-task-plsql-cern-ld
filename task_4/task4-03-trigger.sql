782440e6fb520aea3fb134e1c456fbfbb5354a34
maany <imptodefeat@gmail.com>
2021-07-05 18:35:34 +0200

    cursor test

--- task_4/task4-03-trigger.sql
+++ task_4/task4-03-trigger.sql
@@ -1,14 +1,27 @@
 create or replace trigger check_active_settings_before_new_settings
-    before update
+    before insert or update
     on settings
     for each row
 declare
     l_log_message varchar(256);
     l_active_setting_id number; --settings.setting_id%type;
+    cursor cur_active_settings_per_param_process is
+        select * from ACTIVE_SETTING_PER_PARAM_PROCESS where PARAMETER_ID=:new.parameter_id
+                                                         and process_id = :new.process_id;
+    l_cur_row cur_active_settings_per_param_process%ROWTYPE;
 begin
     l_log_message := 'Trigger: [ parameter_id: ' || :new.parameter_id || ', process_id:' || :new.process_id || ']';
-    dbms_output.put_line(l_log_message);
-    l_active_setting_id := task4.GET_ACTIVE_SETTING(:new.PARAMETER_ID, :new.SETTING_ID);
+    open cur_active_settings_per_param_process;
+--     loop
+--         fetch cur_active_settings_per_param_process into l_cur_row;
+--         exit when cur_active_settings_per_param_process%NOTFOUND;
+--         dbms_output.put_line(l_log_message || ' active setting is: ' || l_cur_row.current_active_setting_id);
+--     end loop;
+--     dbms_output.put_line(l_log_message || ' total rows parsed: ' || cur_active_settings_per_param_process%ROWCOUNT);
+--
+    close cur_active_settings_per_param_process;
+
+--     l_active_setting_id := task4.GET_ACTIVE_SETTING(:new.PARAMETER_ID, :new.SETTING_ID);
 --     if l_active_setting_id = :new.setting_id then
 --         raise_application_error( -20202, 'Setting ' || l_active_setting_id  || ' is already active for ' || l_log_message);
 --     end if;
