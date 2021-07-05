6d3426ad8c8d7e03df2dcab759363c793fb2a817
maany <imptodefeat@gmail.com>
2021-07-05 03:33:27 +0200

    add function call

--- 01-04-t8_function.sql
+++ 01-04-t8_function.sql
@@ -18,7 +18,7 @@ create type task_8_tf_tab is table of task8_tf_row;
 create or replace function render_task_8(
     i_parameter_name IN varchar2,
     i_process_name IN varchar2
-) return task8_tf_row pipelined
+) return task_8_tf_tab pipelined
     as
     l_is_active char(1);
     l_value_type varchar2(30);
@@ -26,7 +26,11 @@ create or replace function render_task_8(
     l_x_values val_array;
     l_y_values val_array;
 begin
-    
+
     pipe row ( task8_tf_row(l_is_active, l_value_type, l_scalar_value, l_x_values, l_y_values ));
     return;
 end;
+
+/
+select * from Table(render_task_8(1,1))
+/
